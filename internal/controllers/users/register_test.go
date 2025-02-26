dcadd0a6e8eb97a2bf53f7762cb9d860c9fb5fb8
GeekchanskiY <astrovskiy.04@gmail.com>
2025-02-26 17:52:19 +0300

    testcontainers init

--- internal/controllers/users/register_test.go
+++ internal/controllers/users/register_test.go
@@ -1,10 +1,48 @@
 package users
 
 import (
+	"context"
+	"fmt"
+	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestController_RegisterUser(t *testing.T) {
+	ctx := context.Background()
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:15.3-alpine",
+		postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
+		postgres.WithDatabase("test-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	assert.NoError(t, err)
+
+	db, err := sqlx.Connect("postgres", connStr)
+	assert.NoError(t, err)
+	fmt.Println(db)
+
+	t.Cleanup(func() {
+		if err := pgContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate pgContainer: %s", err)
+		}
+	})
 	// TODO: split handlers and controllers for more comfortable testing
 	t.Run("success", func(t *testing.T) {
 	})
