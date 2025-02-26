1a43f25f0c2280479d5aea9ff9cf41548ebd5faf
GeekchanskiY <astrovskiy.04@gmail.com>
2025-02-26 17:48:08 +0300

    upd

---
+++ internal/controllers/users/register_test.go
@@ -0,0 +1,13 @@
+package users
+
+import (
+	"testing"
+)
+
+func TestController_RegisterUser(t *testing.T) {
+	// TODO: split handlers and controllers for more comfortable testing
+	t.Run("success", func(t *testing.T) {
+	})
+
+	t.Run("invalid user", func(t *testing.T) {})
+}
