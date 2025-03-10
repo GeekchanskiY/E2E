90ed175f93f6eef3dacd14e2d03777f8bfd0bfa9
GeekchanskiY <astrovskiy.04@gmail.com>
2025-03-10 13:34:18 +0300

    different wallet handling added

--- internal/handlers/frontend/wallet.go
+++ internal/handlers/frontend/wallet.go
@@ -3,11 +3,19 @@ package frontend
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
 )
 
 func (h *Handler) Wallet(w http.ResponseWriter, r *http.Request) {
 	h.logger.Debug("frontend.register.handler", zap.String("event", "got request"))
+
+	walletId := chi.URLParam(r, "id")
+	if walletId == "" {
+		h.logger.Error("frontend.wallet.register.handler: walletId is empty")
+		http.Error(w, "walletId is empty", http.StatusBadRequest)
+		return
+	}
 
 	html, templateData, err := h.controller.Wallet(r.Context(), 1)
 	if err != nil {
