30fe024f6989792684b9d997f26c1619f25ffb47
GeekchanskiY <astrovskiy.04@gmail.com>
2025-03-12 11:51:11 +0300

    scripts init

--- cmd/scripts/main.go
+++ cmd/scripts/main.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var (
+		userInput int
+		err       error
+	)
+
+	fmt.Print("E2E scripts \n\n")
+	fmt.Println(`Choose script to run:
+	1 - create superuser
+	2 - fill db with test data
+	3- clear all tables`)
+
+	_, err = fmt.Scanf("%d", &userInput)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(userInput, err)
 }
