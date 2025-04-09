f277707c188641d2a6b9231c2b831f672b0c9193
GeekchanskiY <astrovskiy.04@gmail.com>
2025-04-09 15:14:25 +0300

    templates divided to folders

--- internal/templates/embed.go
+++ internal/templates/embed.go
@@ -8,23 +8,36 @@ import (
 var Fs embed.FS
 
 const (
-	BaseTemplate                     = "base.gohtml"
-	FinanceTemplate                  = "finance.gohtml"
-	IndexTemplate                    = "index.gohtml"
-	LoginTemplate                    = "login.gohtml"
-	RegisterTemplate                 = "register.gohtml"
-	UserTemplate                     = "user.gohtml"
-	WalletTemplate                   = "wallet.gohtml"
-	CreateWalletTemplate             = "create_wallet.gohtml"
-	CreateDistributorTemplate        = "create_distributor.gohtml"
-	CreateOperationTemplate          = "create_operation.gohtml"
-	CreateOperationGroupTemplate     = "create_operation_group.gohtml"
-	UIKitTemplate                    = "ui_kit.gohtml"
-	PageNotFoundTemplate             = "page_not_found.gohtml"
-	FaqTemplate                      = "faq.gohtml"
-	CreateWorkTemplate               = "create_work.gohtml"
-	WorkTimeTemplate                 = "work_time.gohtml"
-	AddUserToPermissionGroupTemplate = "add_user_to_permission_group.gohtml"
-	CreatePermissionGroupTemplate    = "create_permission_group.gohtml"
-	PermissionGroupsTemplate         = "permission_groups.gohtml"
+
+	// base templates
+
+	BaseTemplate         = "base/base.gohtml"
+	IndexTemplate        = "base/index.gohtml"
+	FaqTemplate          = "base/faq.gohtml"
+	UIKitTemplate        = "base/ui_kit.gohtml"
+	LoginTemplate        = "base/login.gohtml"
+	RegisterTemplate     = "base/register.gohtml"
+	UserTemplate         = "base/user.gohtml"
+	PageNotFoundTemplate = "base/page_not_found.gohtml"
+
+	// finance templates
+
+	FinanceTemplate              = "finance/finance.gohtml"
+	WalletTemplate               = "finance/wallet.gohtml"
+	CreateWalletTemplate         = "finance/create_wallet.gohtml"
+	CreateDistributorTemplate    = "finance/create_distributor.gohtml"
+	CreateOperationTemplate      = "finance/create_operation.gohtml"
+	CreateOperationGroupTemplate = "finance/create_operation_group.gohtml"
+
+	// work templates
+
+	CreateWorkTemplate = "work/create_work.gohtml"
+	WorkTimeTemplate   = "work/work_time.gohtml"
+
+	// permission groups templates
+
+	AddUserToPermissionGroupTemplate = "permissions/add_user_to_permission_group.gohtml"
+	CreatePermissionGroupTemplate    = "permissions/create_permission_group.gohtml"
+	PermissionGroupsTemplate         = "permissions/permission_groups.gohtml"
+	PermissionGroupTemplate          = "permissions/permission_group.gohtml"
 )
